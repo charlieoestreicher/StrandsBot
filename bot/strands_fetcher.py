d5f286fb9513b60975b9e9952562588bb58d1d55
charlieoestreicher <charlie.oestreicher@gmail.com>
2024-09-24 21:41:08 -0500

    Loads in the grid

--- bot/strands_fetcher.py
+++ bot/strands_fetcher.py
@@ -1,6 +1,46 @@
-import logging
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+from config.logger import logger
+
+# 8x6 grid
+driver = webdriver.Chrome()
+
+
+def open_grid_board():
+    driver.get("https://www.nytimes.com/games/strands")
+    start_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (
+                By.CSS_SELECTOR,
+                "button._momentButton_m3x3m_1._primary_m3x3m_113._default_m3x3m_1",
+            )
+        )
+    )
+    start_button.click()
+
+    close_button = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "xwd__modal--close"))
+    )
+    close_button.click()
+
+
+def retrieve_grid():
+    board = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "styles-module_board__D_Xt6"))
+    )
+    char_list = board.text.strip().split("\n")
+    grid = [char_list[i : i + 6] for i in range(0, len(char_list), 6)]
+    return grid
 
 
 def fetch_daily_grid():
-
-    return 0
+    logger.info("opening grid board")
+    open_grid_board()
+    logger.info("retrieving grid")
+    grid = retrieve_grid()
+    prettyGrid = "\n".join([" ".join(row) for row in grid])
+    logger.info(f"successfully retrieved grid:\n{prettyGrid}")
+    return grid
--- main.py
+++ main.py
@@ -3,12 +3,26 @@ from bot.strands_solver import solve_strands
 from bot.strands_display import display_solutions
 from config.logger import logger
 
+env = "d"
+
 
 def main():
-    logger.info("starting")
+    logger.info("NYT Strands bot starting")
 
     # Fetch the daily grid
-    grid = fetch_daily_grid()
+    if env == "d":
+        grid = [
+            ["R", "A", "M", "S", "I", "L"],
+            ["I", "T", "C", "N", "E", "A"],
+            ["A", "N", "I", "O", "B", "O"],
+            ["O", "E", "N", "R", "E", "T"],
+            ["I", "R", "D", "C", "F", "I"],
+            ["D", "R", "G", "U", "M", "C"],
+            ["B", "O", "T", "A", "I", "T"],
+            ["Y", "C", "N", "T", "O", "N"],
+        ]
+    else:
+        grid = fetch_daily_grid()
 
     # Solve the puzzle
     solutions = solve_strands(grid)
